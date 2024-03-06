49764e2d16b65898e29201a493f984042cd00564
Nguyễn Minh Hưng <84146687+hungcoi2002tx@users.noreply.github.com>
2024-03-06 22:20:27 +0700

    ve them hinh

---
+++ app/src/main/java/com/fpt/hungnm/assigmentfinal/MainBudget.java
@@ -0,0 +1,14 @@
+package com.fpt.hungnm.assigmentfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainBudget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_budget);
+    }
+}
