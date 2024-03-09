9a80473ab389cb7b90f1493fd61604c5ea944445
Nguyễn Minh Hưng <84146687+hungcoi2002tx@users.noreply.github.com>
2024-03-09 21:57:13 +0700

    ve them hinh

---
+++ app/src/main/java/com/fpt/hungnm/assigmentfinal/Create_Budget.java
@@ -0,0 +1,14 @@
+package com.fpt.hungnm.assigmentfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_Budget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_budget);
+    }
+}
